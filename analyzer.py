abaeabee6924b425189663d250d257c661611657
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-07-20 13:39:50 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,3 +2,7 @@ import requests
 
 def analyze_github_profile(username):
     # Make a GET request to the GitHub API to fetch user information
+ response = requests.get(f"https://api.github.com/users/{username}")
+    
+    if response.status_code == 200:
+        user_data = response.json()
